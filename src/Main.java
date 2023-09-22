f8c1d83becec590c3ff70619965d0c141a8a2cc3
wilker-eng <w.martins@campus.fct.unl.pt>
2023-09-22 12:58:36 +0100

    wilker method

--- src/Main.java
+++ src/Main.java
@@ -151,7 +151,6 @@ public class Main {
         }
         else System.out.println(BOOK_EMPTY);
     }
-<<<<<<< HEAD
 
     
     private static String SamePhone(Scanner in)
@@ -159,28 +158,34 @@ public class Main {
         int phone;
         phone = in.nextInt(); in.nextLine();
 
-        String contName;
-
         if (cBook.getNumberOfContacts() != 0) 
         {
             cBook.initializeIterator();
 
-            while( cBook.hasNext() )    
+            while( cBook.hasNext() ) 
             {
-                Contact c = cBook.next(); 
-
+                Contact c = cBook.next();
                 String cphone = c.getPhone();
                 if ( phone == cphone )
-                    contName = c.getName;
+                System.out.println(c.getName() + "; " + c.getEmail() + "; " + c.getPhone());
             }
-            System.out.println(cbook.getName(contName) + "; " + cBook.getEmail() + "; " + cBook.getPhone());
-
         }
-         if (cphone == NULL)
-            System.out.println( PHONE_NOTEXIST);
-         
 
     }
-=======
->>>>>>> 35a3d993e72d80f2f3754db5fe565bebf6a6a990
+    private static void givingNumber(Scanner in, ContactBook cBook) {
+        int number = in.nextInt();
+        Contact result = null;
+        cBook.initializeIterator();
+        while (cBook.hasNext()) {
+            Contact c = cBook.next();
+            if (c.getPhone() == number) {
+                result = c;
+            }
+        }
+        if (result != null) {
+            System.out.println(result.getName() + "; " + result.getEmail() + "; " + result.getPhone());
+        } else {
+            System.out.println("Phone number does not exist.");
+        }
+    }
 }
